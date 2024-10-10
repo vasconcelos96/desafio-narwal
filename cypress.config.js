a7bf6e91e101e409620bed6bad6b89c676f07090
Daniel Vasconcelos <daniel-suporte.tecnico.ti@outlook.com>
2024-10-10 20:19:46 -0300

    Update

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    viewportWidth: 1280,   // Largura do viewport
+    viewportHeight: 720, 
+    baseUrl: 'https://www.narwalsistemas.com.br/',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+  env: {
+    GITHUB_TOKEN: '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', 
+  },
+});
